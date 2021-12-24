02902925a85abdfce12060e18941a5f761b5af29
Dmitri Popov <dmpop@linux.com>
2021-12-24 12:26:20 +0100

    Minor cometic tweaks

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-    // Theme (light, dark, sepia)
-    $theme = "dark";
-    // Footer
-    $footer = "Read the <a href='https://dmpop.gumroad.com/l/linux-photography'>Linux Photography</a> book.";
-?>
--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
-include('config.php');
+    // Theme (light, dark, sepia)
+    $theme = "dark";
+    // Footer
+    $footer = "Read the <a href='https://dmpop.gumroad.com/l/linux-photography'>Linux Photography</a> book.";
 ?>
 
 <!DOCTYPE html>
